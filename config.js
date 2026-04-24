0f04b1302b7a252acc9028497ee511f4bde9758d
sehrawatmanish07 <manishsehrawat13808@gmail.com>
2026-04-24 14:59:57 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
 // API Configuration
 window.ENV_API_BASE = 'https://nestify-backend-pkia.onrender.com/api';
+
+// Wake up Render server on page load
+fetch('https://nestify-backend-pkia.onrender.com/api/health')
+  .then(() => console.log('Server is awake'))
+  .catch(() => console.log('Waking server up...'));
